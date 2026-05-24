e541095bd5ef63c629cd14ebe592e249bb1e695d
captainsirk1-commits <captainsirk1@gmail.com>
2026-05-24 16:39:42 -0600

    Near clean slate/initial commit
    Brand new Unity project. A scripts folder has been added, and a virtually empty scriptable object script created.

---
+++ Assets/Scripts/PlanetSettings.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+//Determines global settings. Maximum sea level, average temperature, ect...
+
+[CreateAssetMenu(fileName = "PlanetSettings", menuName = "Scriptable Objects/PlanetSettings")]
+public class PlanetSettings : ScriptableObject
+{
+    
+}
